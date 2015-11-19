1a32bc274b8343957c41be1c56833d7d28902a75
marynate <mary.w.nate@gmail.com>
2015-11-19 18:06:53 +0800

    Remove redundant include from sample main.cpp

--- sample/src/main.cpp
+++ sample/src/main.cpp
@@ -11,9 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 #include "game.h"
-#include "fplbase/utilities.h"
 
 extern "C" int FPL_main(int argc, char* argv[]) {
   scene_lab_sample::Game game;
